1cae696e340e73c21748845f195859d810f0bdc7
Walker <hiwangchi@gmail.com>
2016-09-26 23:55:06 +0800

    add sub components and sass-loader

---
+++ components/SiteHeader.vue
@@ -0,0 +1,84 @@
+<template lang="html">
+  <header class="site-header">
+    <div class="site-header-container">
+      <div class="logo">
+        <h1 class="text-logo"><a href="/">React Webpack</a></h1>
+      </div>
+      <ul class="top-menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/page1">Page1</a></li>
+        <li><a href="/page2">Page2</a></li>
+      </ul>
+      <ul class="top-menu top-login">
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  ready () {},
+  attached () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="sass">
+.site-header {
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px rgba(255, 255, 255, 0.15) solid;
+  background-color: #24282C;
+
+  .site-header-container {
+    margin: 0 auto;
+    width: 1200px;
+    background-color: #24282C;
+    overflow: hidden;
+
+    .logo {
+      float: left;
+      .text-logo {
+        font-size: 22px;
+        line-height: 60px;
+
+        a {
+          color: #fff;
+          &:hover {
+            opacity: .8;
+          }
+        }
+      }
+    }
+
+    .top-menu {
+      float: left;
+      margin-left: 100px;
+    }
+
+    li {
+      float: left;
+      a {
+        display: block;
+        padding: 0 20px;
+        line-height: 60px;
+        color: #fff;
+
+        &:hover {
+          opacity: .8;
+        }
+      }
+    }
+
+    .top-login {
+      float: right;
+    }
+  }
+}
+</style>
--- pages/App.vue
+++ pages/App.vue
@@ -1,20 +1,28 @@
 <template lang="html">
+  <site-header></site-header>
   <div class="message">
     {{ msg }}
   </div>
 </template>
 
 <script>
+  import SiteHeader from '../components/SiteHeader.vue';
   export default {
     data: function () {
       return {
         msg: 'Hello from vue-loader'
       }
+    },
+    components: {
+      SiteHeader
     }
   }
 </script>
 
-<style lang="css">
+<style lang="sass">
+  @import '../styles/reset.scss';
+  @import '../styles/common.scss';
+
   .message {
     color: #f80;
     font-size: 14px;
