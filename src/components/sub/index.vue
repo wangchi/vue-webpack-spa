1835e9944e8cc6474962572a2c8a57e7f092b8b1
Walker <hiwangchi@gmail.com>
2016-11-16 23:54:57 +0800

    Parent-Child communication

---
+++ src/components/sub/index.vue
@@ -0,0 +1,34 @@
+/**
+ * Site Footer
+ */
+
+<template lang="html">
+  <div class="sub">
+    <input type="text" v-model="msg">
+    <button @click="sendMessage">Sub, Dispatch</button>
+    <br>
+    {{ subData }}
+  </div>
+</template>
+
+<script>
+  import './index.styl';
+
+  export default {
+    data () {
+      return {
+        msg: 'hello',
+        subData: 'sub'
+      }
+    },
+    computed: {},
+    ready () {},
+    attached () {},
+    methods: {
+      sendMessage: function () {
+        this.$dispatch('send-message', this.msg);
+      }
+    },
+    components: {}
+  }
+</script>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,10 @@
 <template lang="html">
   <site-header></site-header>
   <div class="content">
-    <div class="login placeholder">Login</div>
+    <div class="login placeholder">
+      {{msg}}
+      <sub></sub>
+    </div>
   </div>
   <site-footer></site-footer>
 </template>
@@ -11,15 +14,30 @@
 
   import SiteHeader from '../../components/site-header';
   import SiteFooter from '../../components/site-footer';
+  import Sub from '../../components/sub';
+
   export default {
     data: function () {
       return {
-        msg: 'Home'
+        msg: 'Login'
+      }
+    },
+    methods: {
+      print: function () {
+        console.log('print fn');
+      }
+    },
+    events: {
+      'send-message': function (data) {
+        // console.log(data);
+        this.msg = data;
+        this.print();
       }
     },
     components: {
       SiteHeader,
-      SiteFooter
+      SiteFooter,
+      Sub
     }
   }
 </script>
